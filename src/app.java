3fa8f8f5a75db28b9f5c655e3e3621b6c2271085
swara2402 <swaralimahishi24@gmail.com>
2026-01-30 21:11:25 +0530

    Added changes by Swarali

--- src/app.java
+++ src/app.java
@@ -1,5 +1,5 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello from Maven + Jenkins!");
+        System.out.println("Hello we are Team SKY");
     }
 }
